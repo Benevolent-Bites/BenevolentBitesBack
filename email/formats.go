e85e374a435fda1e75c82238ae10397c297f75c5
Rishabh Bector <bector.rishabh@gmail.com>
2020-05-01 16:13:15 -0500

    minor changes

--- email/formats.go
+++ email/formats.go
@@ -4,7 +4,7 @@ var ReportFormat = `
 
 	Hi %s,
 
-		This week, you've earned $%v from Benevolent Bites! This is from credit that your employer, %s, sold on our nonprofit platform. Your manager is responsible for paying you these dues.
+		This week, you've earned $ %v from Benevolent Bites! This is from credit that your employer, %s, sold on our nonprofit platform. Your manager is responsible for paying you these dues.
 
 		Thanks,
 		
